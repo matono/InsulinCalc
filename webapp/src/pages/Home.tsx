1d160cec3f00fad749b1488a6fa829026b0ee273
MATONO Akiyoshi <a.matono@aist.go.jp>
2023-04-13 11:39:10 +0900

    Layout using Grid (#4)

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,11 +1,158 @@
 import React from 'react';
-import { Container } from "@mui/material";
+import { Box, Card, Grid, Typography} from "@mui/material";
+import Item from '../components/Item';
+import '@fontsource/roboto';
+
 
 const Home: React.FC = () => {
   return (
-    <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
-      <h2>Home</h2>
-    </Container>
+    //<Container fixed>
+    <>
+      <Box >
+        <Grid container>
+
+          <Grid xs={12}>
+            <Typography variant="h2">補正インスリン</Typography>
+          </Grid>
+
+          {/* 補正インスリン式 */}
+          <Grid container xs={12}>
+
+            {/* 左辺 */}
+            <Grid container xs={8}>
+              {/* 分母 */}
+              <Grid container columns={21}>
+                <Grid xs={10}>
+                  <Item>食前血糖値</Item>
+                </Grid>
+                <Grid xs={1}>
+                  <Item>-</Item>
+                </Grid>
+                <Grid xs={10}>
+                  <Item>目標血糖値</Item>
+                </Grid>
+              </Grid>           
+
+              <Grid container>
+                <Grid xs={12}>
+                  <Item>-----</Item>
+                </Grid>
+              </Grid>           
+
+              <Grid container>
+                <Grid xs={12}>
+                  <Item>インスリン効果比</Item>
+                </Grid>
+              </Grid>           
+            </Grid>
+
+            {/* =右辺 */}
+            <Grid container xs={4}>
+
+              {/* = */}
+              <Grid container xs={1}>
+                  <Item>=</Item>
+              </Grid>
+
+              {/* 右辺 */}
+              <Grid container xs={11}>
+                <Item>右辺</Item>
+              </Grid>
+
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box>
+        <Grid container>
+
+          <Grid xs={12}>
+           <Typography variant="h2">糖質インスリン</Typography>
+          </Grid>
+
+          {/* 補正インスリン式 */}
+          <Grid container xs={12}>
+
+            {/* 左辺 */}
+            <Grid container xs={8}>
+
+              <Grid container columns={21}>
+                <Grid xs={10}>
+                  <Item>カーボ数</Item>
+                </Grid>
+                <Grid xs={1}>
+                  <Item>x</Item>
+                </Grid>
+                <Grid xs={10}>
+                  <Item>カーボ比</Item>
+                </Grid>
+              </Grid>              
+            </Grid>
+
+            {/* =右辺 */}
+            <Grid container xs={4}>
+
+              {/* = */}
+              <Grid container xs={1}>
+                  <Item>=</Item>
+              </Grid>
+
+              {/* 右辺 */}
+              <Grid container xs={11}>
+                <Item>右辺</Item>
+              </Grid>
+
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box>
+        <Grid container>
+
+          <Grid xs={12}>
+           <Typography variant="h2">合計インスリン</Typography>
+          </Grid>
+
+          {/* 補正インスリン式 */}
+          <Grid container xs={12}>
+
+            {/* 左辺 */}
+            <Grid container xs={8}>
+
+              <Grid container columns={21}>
+                <Grid xs={10}>
+                  <Item>補正</Item>
+                </Grid>
+                <Grid xs={1}>
+                  <Item>+</Item>
+                </Grid>
+                <Grid xs={10}>
+                  <Item>糖質</Item>
+                </Grid>
+              </Grid>              
+            </Grid>
+
+            {/* =右辺 */}
+            <Grid container xs={4}>
+
+              {/* = */}
+              <Grid container xs={1}>
+                  <Item>=</Item>
+              </Grid>
+
+              {/* 右辺 */}
+              <Grid container xs={11}>
+                <Item>右辺</Item>
+              </Grid>
+
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+    //</Container>
   );
 };
 
