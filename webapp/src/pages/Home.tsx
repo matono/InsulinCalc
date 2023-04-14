b881d6bac9d361a17b27416c1875497cd8b2cf68
MATONO Akiyoshi <a.matono@aist.go.jp>
2023-04-14 12:55:55 +0900

    Refine design a bit(#6)

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Box, Paper, Container, Typography, TextField} from "@mui/material";
 import CssBaseline from '@mui/material/CssBaseline';
 
-import { Unstable_Grid2 as Grid } from '@mui/material'; // Grid version 2
+//import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Grid from '@mui/material/Grid';
 import '@fontsource/roboto';
 //import Item from '../components/Item';
 import { NumericFormat } from 'react-number-format';
@@ -31,16 +32,7 @@ const Item = styled(Box)(({ theme }) => ({
 
 const theme = createTheme();
 
-const Home: React.FC = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-  
+const Home: React.FC = () => {  
   return (
     <ThemeProvider theme={theme}>
       
@@ -53,188 +45,336 @@ const Home: React.FC = () => {
           }}
         >
           
-          <Typography component="h1" variant="h4" gutterBottom>インスリン</Typography>
+          <Typography component="h1" variant="body1" gutterBottom>インスリン</Typography>
 
-          <Paper component="form" onSubmit={handleSubmit} sx={{ mt: 3, p:2 }} >
+          <Paper component="form" sx={{ mt: 3, p:2 }} >
             <Typography component="h2" variant="h5" sx={{ mb: 3}}>糖質インスリン</Typography>
 
             {/* 糖質インスリン式 */}
             {/* 左辺 */}
-            <Grid container columns={11}>
-              <Grid xs={5}>
-                <NumericFormat 
-                  id="meal_carbohydrates" 
-                  value={10.00}
-                  type="text"
-                  valueIsNumericString={true}
-                  decimalSeparator="."
-                  thousandSeparator=","
-                  allowNegative={false}
-                  decimalScale={2}
-                  fixedDecimalScale              
-                  inputProps={{ 
-                    inputMode: 'decimal', 
-                    pattern: '[0-9].*' 
-                  }}
-                  customInput={TextField} 
-                  label="カーボ数" 
-                  variant="outlined" 
-                  fullWidth
-                    />
-              </Grid>
-              
-              <Grid xs={1}>
-                <Item>
-                  <Typography variant="body1">x</Typography>
-                </Item>
-              </Grid>
+            <Grid container rowSpacing={2} columnSpacing={0}>
+              <Grid item xs={12}>
+                <Grid container columns={11}>
+                  <Grid xs={5}>
+                    <NumericFormat 
+                      id="meal_carbohydrates" 
+                      value={10.00}
+                      type="text"
+                      valueIsNumericString={true}
+                      decimalSeparator="."
+                      thousandSeparator=","
+                      allowNegative={false}
+                      decimalScale={2}
+                      fixedDecimalScale              
+                      inputProps={{ 
+                        inputMode: 'decimal', 
+                        pattern: '[0-9].*' 
+                      }}
+                      customInput={TextField} 
+                      label="カーボ数" 
+                      variant="outlined" 
+                      fullWidth
+                        />
+                  </Grid>
+                  
+                  <Grid item xs={1}>
+                    <Item>
+                      <Typography variant="body1">x</Typography>
+                    </Item>
+                  </Grid>
 
-              <Grid xs={5}>
-                <NumericFormat 
-                  id="insulin_to_carb_ratio" 
-                  value={1.0}
-                  type="text"
-                  valueIsNumericString={true}
-                  decimalSeparator="."
-                  thousandSeparator=","
-                  allowNegative={false}
-                  decimalScale={1}
-                  fixedDecimalScale              
-                  inputProps={{ 
-                    inputMode: 'decimal', 
-                    pattern: '[0-9].*' 
-                  }}
-                  customInput={TextField} 
-                  label="カーボ比" 
-                  variant="outlined" 
-                  fullWidth
-                    />
-              </Grid>
-            </Grid>           
-
-            {/* =右辺 */}
-            <Grid container xs={12}>
-
-              {/* = */}
-              <Grid container xs={1}>
-                  <Item>
-                    <Typography variant="body1">=</Typography>
-                  </Item>
+                  <Grid item xs={5}>
+                    <NumericFormat 
+                      id="insulin_to_carb_ratio" 
+                      value={1.0}
+                      type="text"
+                      valueIsNumericString={true}
+                      decimalSeparator="."
+                      thousandSeparator=","
+                      allowNegative={false}
+                      decimalScale={1}
+                      fixedDecimalScale              
+                      inputProps={{ 
+                        inputMode: 'decimal', 
+                        pattern: '[0-9].*' 
+                      }}
+                      customInput={TextField} 
+                      label="カーボ比" 
+                      variant="outlined" 
+                      fullWidth
+                        />
+                  </Grid>
+                </Grid>
               </Grid>
 
-              {/* 右辺 */}
-              <Grid container xs={10}>
-                  <Item>
-                    <Typography variant="body1">10.0</Typography>
-                  </Item>
+              <Grid item xs={12}>
+                <Grid container columns={11}>
+                  {/* = */}
+                  <Grid item xs={1}>
+                      <Item>
+                        <Typography variant="body1">=</Typography>
+                      </Item>
+                  </Grid>
+
+                  {/* 右辺 */}
+                  <Grid item xs={10}>
+                    <NumericFormat 
+                      id="carb_insulin" 
+                      value={1.0}
+                      type="text"
+                      valueIsNumericString={true}
+                      decimalSeparator="."
+                      thousandSeparator=","
+                      allowNegative={false}
+                      decimalScale={1}
+                      fixedDecimalScale              
+                      inputProps={{ 
+                        inputMode: 'decimal', 
+                        pattern: '[0-9].*' 
+                      }}
+                      customInput={TextField} 
+                      label="糖質インスリン" 
+                      variant="outlined" 
+                      fullWidth
+                        />
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Paper>
 
-
-
-          <Paper component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3, p:2 }}>
+          <Paper component="form" sx={{ mt: 3, p:2 }}>
            <Typography component="h2" variant="h5" sx={{ mb: 3}}>補正インスリン</Typography>
 
             {/* 補正インスリン式 */}
-            {/* 左辺 */}
-            <Grid container columns={11}>
-              {/* 分子 */}
-              <Grid xs={5}>
-                <NumericFormat 
-                  id="current_BGL" 
-                  value={100}
+            <Grid container rowSpacing={2} columnSpacing={0}>
+              {/* 左辺 */}
+              <Grid item xs={12}>
+                
+                {/* 分数 */}
+                <Grid container>
+                  {/* 分子 */}
+                  <Grid item xs={12}>
+                    <Grid container columns={11}>
+                      <Grid item xs={5}>
+                        <NumericFormat 
+                          id="current_BGL" 
+                          value={100}
 
-                  valueIsNumericString={true}
-                  decimalSeparator="."
-                  thousandSeparator=","
-                  allowNegative={false}
-                  decimalScale={0}
-                  fixedDecimalScale              
-                  inputProps={{ 
-                    inputMode: 'numeric', 
-                    pattern: '[0-9]*' 
-                  }}
-                  customInput={TextField} 
-                  label="食前血糖値" 
-                  variant="outlined" 
-                  fullWidth
-                    />
+                          valueIsNumericString={true}
+                          decimalSeparator="."
+                          thousandSeparator=","
+                          allowNegative={false}
+                          decimalScale={0}
+                          fixedDecimalScale              
+                          inputProps={{ 
+                            inputMode: 'numeric', 
+                            pattern: '[0-9]*' 
+                          }}
+                          customInput={TextField} 
+                          label="食前血糖値" 
+                          variant="outlined" 
+                          fullWidth
+                            />
+                      </Grid>
+
+                      <Grid item xs={1}>
+                        <Item>
+                          <Typography variant="body1">-</Typography>
+                        </Item>
+                      </Grid>
+
+                      <Grid item xs={5}>
+                        <NumericFormat 
+                          id="target_BGL" 
+                          value={120}
+
+                          valueIsNumericString={true}
+                          decimalSeparator="."
+                          thousandSeparator=","
+                          allowNegative={false}
+                          decimalScale={0}
+                          fixedDecimalScale              
+                          inputProps={{ 
+                            inputMode: 'numeric', 
+                            pattern: '[0-9]*' 
+                          }}
+                          customInput={TextField} 
+                          label="目標血糖値" 
+                          variant="outlined" 
+                          fullWidth
+                            />
+                      </Grid>
+                    </Grid>
+                  </Grid>
+
+                  {/* 分数線 */}
+                  <Grid item xs={12}>
+                    <Line></Line>
+                  </Grid>      
+
+                  {/* 分母 */}
+                  <Grid item xs={12}>
+                    <NumericFormat 
+                      id="insulin_sensitivity_factor" 
+                      value={80}
+
+                      valueIsNumericString={true}
+                      decimalSeparator="."
+                      thousandSeparator=","
+                      allowNegative={false}
+                      decimalScale={0}
+                      fixedDecimalScale              
+                      inputProps={{ 
+                        inputMode: 'numeric', 
+                        pattern: '[0-9]*' 
+                      }}
+                      customInput={TextField} 
+                      label="インスリン効果比" 
+                      variant="outlined" 
+                      fullWidth
+                        />
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid xs={1}>
-                <Item>
-                  <Typography variant="body1">-</Typography>
-                </Item>
-              </Grid>
-              <Grid xs={5}>
-                <NumericFormat 
-                  id="target_BGL" 
-                  value={120}
 
-                  valueIsNumericString={true}
-                  decimalSeparator="."
-                  thousandSeparator=","
-                  allowNegative={false}
-                  decimalScale={0}
-                  fixedDecimalScale              
-                  inputProps={{ 
-                    inputMode: 'numeric', 
-                    pattern: '[0-9]*' 
-                  }}
-                  customInput={TextField} 
-                  label="目標血糖値" 
-                  variant="outlined" 
-                  fullWidth
-                    />
-              </Grid>
+              {/* =右辺 */}
+              <Grid item xs={12}>
+                <Grid container columns={11}>
+                  {/* = */}
+                  <Grid item xs={1}>
+                    <Item>
+                      <Typography variant="body1">=</Typography>
+                    </Item>
+                  </Grid>
 
-              {/* 分数線 */}
-              <Grid container xs={21}>
-                <Line></Line>
-              </Grid>      
-
-              {/* 分母 */}
-              <Grid container xs={21}>
-                <NumericFormat 
-                  id="insulin_sensitivity_factor" 
-                  value={80}
-
-                  valueIsNumericString={true}
-                  decimalSeparator="."
-                  thousandSeparator=","
-                  allowNegative={false}
-                  decimalScale={0}
-                  fixedDecimalScale              
-                  inputProps={{ 
-                    inputMode: 'numeric', 
-                    pattern: '[0-9]*' 
-                  }}
-                  customInput={TextField} 
-                  label="インスリン効果比" 
-                  variant="outlined" 
-                  fullWidth
-                    />
-              </Grid>
-            </Grid>
-
-            {/* =右辺 */}
-            <Grid container xs={12}>
-
-              {/* = */}
-              <Grid container xs={1}>
-                  <Item>
-                    <Typography variant="body1">=</Typography>
-                  </Item>
-              </Grid>
-
-              {/* 右辺 */}
-              <Grid container xs={10}>
-                  <Item>
-                    <Typography variant="body1">1.0</Typography>
-                  </Item>
+                  {/* 右辺 */}
+                  <Grid item xs={10}>
+                    <NumericFormat 
+                      id="correction_bolus_insulin" 
+                      value={1.0}
+                      type="text"
+                      valueIsNumericString={true}
+                      decimalSeparator="."
+                      thousandSeparator=","
+                      allowNegative={false}
+                      decimalScale={1}
+                      fixedDecimalScale              
+                      inputProps={{ 
+                        inputMode: 'decimal', 
+                        pattern: '[0-9].*' 
+                      }}
+                      customInput={TextField} 
+                      label="補正インスリン" 
+                      variant="outlined" 
+                      fullWidth
+                        />
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Paper>
+
+          
+
+          <Paper component="form" sx={{ mt: 3, p:2 }} >
+            <Typography component="h2" variant="h5" sx={{ mb: 3}}>合計インスリン</Typography>
+
+
+            {/* 合計インスリン式 */}
+            <Grid container rowSpacing={2} columnSpacing={0}>
+              {/* 左辺 */}
+              <Grid item xs={12}>
+                <Grid container columns={11}>
+                  <Grid xs={5}>
+                    <NumericFormat 
+                      id="meal_carbohydrates" 
+                      value={10.00}
+                      type="text"
+                      valueIsNumericString={true}
+                      decimalSeparator="."
+                      thousandSeparator=","
+                      allowNegative={false}
+                      decimalScale={2}
+                      fixedDecimalScale              
+                      inputProps={{ 
+                        inputMode: 'decimal', 
+                        pattern: '[0-9].*' 
+                      }}
+                      customInput={TextField} 
+                      label="糖質インスリン" 
+                      variant="outlined" 
+                      fullWidth
+                        />
+                  </Grid>
+                  
+                  <Grid item xs={1}>
+                    <Item>
+                      <Typography variant="body1">+</Typography>
+                    </Item>
+                  </Grid>
+
+                  <Grid item xs={5}>
+                    <NumericFormat 
+                      id="insulin_to_carb_ratio" 
+                      value={1.0}
+                      type="text"
+                      valueIsNumericString={true}
+                      decimalSeparator="."
+                      thousandSeparator=","
+                      allowNegative={false}
+                      decimalScale={1}
+                      fixedDecimalScale              
+                      inputProps={{ 
+                        inputMode: 'decimal', 
+                        pattern: '[0-9].*' 
+                      }}
+                      customInput={TextField} 
+                      label="補正インスリン" 
+                      variant="outlined" 
+                      fullWidth
+                        />
+                  </Grid>
+                </Grid>
+              </Grid>  
+
+              {/* =右辺 */}
+              <Grid item xs={12}>
+                <Grid container columns={11}>
+                  {/* = */}
+                  <Grid item xs={1}>
+                    <Item>
+                      <Typography variant="body1">=</Typography>
+                    </Item>
+                  </Grid>
+
+                  {/* 右辺 */}
+                  <Grid item xs={10}>
+                    <NumericFormat 
+                      id="total_insulin" 
+                      value={1.0}
+                      type="text"
+                      valueIsNumericString={true}
+                      decimalSeparator="."
+                      thousandSeparator=","
+                      allowNegative={false}
+                      decimalScale={1}
+                      fixedDecimalScale              
+                      inputProps={{ 
+                        inputMode: 'decimal', 
+                        pattern: '[0-9].*' 
+                      }}
+                      customInput={TextField} 
+                      label="合計インスリン" 
+                      variant="outlined" 
+                      fullWidth
+                        />
+                  </Grid>
+                </Grid>
+              </Grid>  
+            </Grid>
+          </Paper>
+
         </Box>
       </Container>
 
